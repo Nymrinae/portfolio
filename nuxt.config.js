5ce70728109a67628db4ce4ef4b859134cdd8b92
Nymrinae <nymrinaedev@gmail.com>
2020-07-30 22:25:03 +0200

    Setup meta tags && update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,14 +16,20 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Sylvain RAYA',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Mon portfolio' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:url', name: 'og:url', content: process.env.WEBSITE_URL },
+      { hid: 'og:title', name: 'og:title', content: 'Portfolio' },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'Portfolio' },
+      { hid: 'og:locale', name: 'og:locale', content: 'fr' },
+      { hid: 'og:image', name: 'og:image', content: `https://cdn.discordapp.com/attachments/656052224689963021/736589688101535844/0.png` },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/icon', href: '/favicon.ico' }
     ]
   },
   /*
