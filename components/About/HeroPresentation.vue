99a2011dae7caeecf60a451f511847fc2d1224fe
Nymrinae <nymrinaedev@gmail.com>
2020-08-01 15:14:24 +0200

    Add CV

--- components/About/HeroPresentation.vue
+++ components/About/HeroPresentation.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="about" class="text-gray-500 bg-gray-900 body-font">
     <div class="max-w-6xl flex items-center h-auto h-screen flex-wrap mx-auto md:mb-0 xl:my-0">
-      <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-gray-900 opacity-75 mx-6 lg:mx-0 mt-24 md:mt-0">
+      <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-gray-900 opacity-75 mx-6 lg:mx-0 mt-24 md:-mt-12">
         <div class="p-4 md:p-12 text-center lg:text-left">
           <h1 class="text-3xl font-bold mt-6 pt-0 md:pt-8 lg:pt-0">Sylvain RAYA <br> {{ $t('TITLE') }}</h1>
           <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
@@ -12,7 +12,11 @@
                 {{ $t('CONTACT.CONTACT_ME') }}
               </button>
             </a>
-            <a>
+            <a
+              :href="`/cv_${$i18n.locale}.pdf`"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button class="inline-flex w-1/2 md:w-auto mb-4 md:mb-0 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 {{ $t('CV') }}
               </button>
