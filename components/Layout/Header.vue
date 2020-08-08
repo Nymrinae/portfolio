520265997444c3da3c59f9f5f0ba1a582bba9608
Nymrinae <nymrinaedev@gmail.com>
2020-08-09 01:30:00 +0200

    Add Start of Mobile Menu

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
-  <header id="navbar" class="text-gray-500 bg-gray-900 body-font w-full fixed z-10">
-    <div class="container mx-auto flex p-5 flex-col md:flex-row items-center">
+  <header
+    id="navbar"
+    class="text-gray-500 bg-gray-900 body-font w-full fixed z-10"
+    :class="{ 'h-full': show }"
+    >
+    <div class="container mx-auto flex p-5 flex-col md:flex-row items-center ">
       <a
         class="flex title-font font-medium items-center mb-4 md:mb-0 hidden md:flex"
         @click="scrollTo('about')"
@@ -8,22 +12,46 @@
         <img src="https://cdn.discordapp.com/attachments/656052224689963021/736589688101535844/0.png" class="w-20 h-20 mr-4 rounded-full hidden md:flex" />
       </a>
       <div class="flex">
-        <span class="ml-3 text-xl text-green-500 float-left w-4/5 md:w-auto"> {{ pseudo }} </span>
+        <span
+          class="ml-3 text-xl text-green-500 float-left w-4/5 md:w-auto"
+          @click="scrollTo('about')"
+        >
+          {{ pseudo }}
+        </span>
         <svg
           class="fill-current h-8 w-8 -mr-4 w-1/5 md:hidden"
           viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          xmlns="http://www.w3.org/2000/svg"
+          v-if="!show"
+          @click="show = !show"
+        >
+          <path :d="show ? 'm243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0' : 'M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'" />
+        </svg>
+        <svg
+          v-if="show"
+          class="fill-current h-6 w-6 -mr-4 w-1/5 md:hidden"
+          viewBox="0 0 365 365"
+          xmlns="http://www.w3.org/2000/svg"
+          @click="show = !show"
+        >
+          <path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0"/>
         </svg>
       </div>
-      <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 hidden md:flex md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-        <a
-          v-for="(link, l) in links"
-          :key="l"
-          class="mr-5 navLink"
-          @click="scrollTo(link.path)"
-          v-html="$t(link.name)"
-        />
+      <nav
+        class="navigation md:mr-auto md:ml-4 md:py-1 md:pl-4 md:flex md:border-l md:border-gray-700 block md:flex md:flex-wrap items-center text-base justify-center align-middle"
+        :class="{ 'hidden': !show }"
+      >
+        <ul>
+          <li>
+            <a
+              v-for="(link, l) in links"
+              :key="l"
+              class="navLink mr-5 w-full md:w-auto align-center mb-8 md:mb-auto"
+              @click="show = !show; scrollTo(link.path)"
+              v-html="$t(link.name)"
+            />
+          </li>
+        </ul>
       </nav>
       <div class="hidden md:flex">
         <LogoHandler :logos="networks" />
@@ -42,8 +70,16 @@ export default class Header extends Vue {
   private readonly pseudo: string = '<Nymrinae />'
   private readonly links: Array<HeaderLinks> = links
   private readonly networks: Array<string> = ['LinkedIn', 'Github', 'Twitter']
+  private show: Boolean = false
 
   private readonly scrollTo: Function = scrollTo
+
+  mounted() {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth >= 767)
+        this.show = false
+    })
+  }
 }
 </script>
 
@@ -77,5 +113,12 @@ export default class Header extends Vue {
 .navLink:hover:after {
   width: 100%;
   left: 0;
+}
+
+@media screen and (max-width: 450px) {
+  .navigation {
+    margin-top: 50%;
+    text-align: center;
+  }
 }
 </style>
