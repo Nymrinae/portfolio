f3e4da4c3608242db12d2a2fe21a4f63c3e1c919
Nymrinae <52325682+Nymrinae@users.noreply.github.com>
2021-03-28 01:52:16 +0100

    Remove Map

--- components/Contact/ContactHandler.vue
+++ components/Contact/ContactHandler.vue
@@ -4,7 +4,6 @@
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 mt-12 text-white"> {{ $t('CONTACT.CONTACT_ME') }} </h1>
     </div>
     <div class="container px-5 pb-24 mx-auto flex sm:flex-no-wrap flex-wrap">
-      <Map />
       <ContactForm />
     </div>
   </section>
